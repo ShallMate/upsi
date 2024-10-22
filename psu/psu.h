923b336925b29e8032d9a3135a6c6bc31f2b2e3e
ShallMate <s200201071@stu.cqupt.edu.cn>
2024-10-22 14:40:40 +0800

    upsi

--- psu/psu.h
+++ psu/psu.h
@@ -1,3 +1,17 @@
+// Copyright 2024 Guowei LING.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//   http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 #pragma once
 
 #include <memory>
@@ -7,7 +21,7 @@
 #include "yacl/link/context.h"
 #include "yacl/secparam.h"
 
-/* security parameter declaration */
+
 YACL_MODULE_DECLARE("psu", SecParam::C::k128, SecParam::S::k40);
 
 uint64_t Evaluate(const std::vector<uint64_t>& coeffs, uint64_t x);
@@ -15,8 +29,6 @@ uint64_t Evaluate(const std::vector<uint64_t>& coeffs, uint64_t x);
 std::vector<uint64_t> Interpolate(const std::vector<uint64_t>& xs,
                                   const std::vector<uint64_t>& ys);
 
-// Scalable Private Set Union from Symmetric-Key Techniques
-// https://eprint.iacr.org/2019/776.pdf (Figure 10)
 
 std::vector<uint128_t> KrtwPsuSend(
     const std::shared_ptr<yacl::link::Context>& ctx,
